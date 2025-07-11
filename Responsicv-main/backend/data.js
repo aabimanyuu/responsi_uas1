b9e8ea610a6bc7c9283d9fd1cf950d2e69647842
aabimanyuu <abimanyusaf@students.amikom.ac.id>
2025-07-11 16:50:39 +0700

    Initial commit

---
+++ Responsicv-main/backend/data.js
@@ -0,0 +1,27 @@
+// backend/data.js
+const educationHistory = [ 
+    { id: 1,
+     period: '2023 - Sekarang',
+     institution: 'Universitas AMIKOM Yogyakarta',
+     major: 'S1 - Teknik Informatika' 
+    },
+    { id: 2,
+     period: '2018 - 2021',
+     institution: 'SMK Negeri 3 Yogyakarta',
+     major: 'Teknik Kendaraan Ringan (Otomotif)' 
+    }
+];
+const skills = [ 
+    { name: 'Vue.js', level: 'Pemula' }, { name: 'JavaScript', level: 'Pemula' },
+    { name: 'Tailwind CSS', level: 'Pemula' }, { name: 'Node.js', level: 'Pemula' },
+    { name: 'Express.js', level: 'Pemula' }, { name: 'PostgreSQL', level: 'Pemula' },
+    { name: 'Git & GitHub', level: 'Pemula' }, { name: 'HTML5 & CSS3', level: 'Pemula' }
+];
+const projects = [ 
+    { title: 'Notifikasi Damage Game', image: '/src/assets/img/Notifikasi.png',
+    description: 'Sistem Notifikasi Damage Game Sederhana', tech: ['C#'
+], link: '#' },
+    { title: 'Simulasi Traffic Light', image: '/src/assets/img/TrafficL.png',
+    description: 'Simulasi traffic light dengan protheus', tech: ['Protheus 8', 'CVAVR'], link: '#' }
+];
+module.exports = { educationHistory, skills, projects };
